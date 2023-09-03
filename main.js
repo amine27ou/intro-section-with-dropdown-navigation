f95a3659f68338e5237f9fa0bc240ead638750a3
amine27ou <amineou433@gmail.com>
2023-09-03 14:37:46 +0100

    auto

---
+++ main.js
@@ -0,0 +1,25 @@
+let links = document.querySelectorAll(".link");
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        let image = link.querySelector("#image");
+        let subLinks = link.querySelector(".sub-links");
+        subLinks.classList.toggle("active");
+        if(subLinks.classList.contains("active")){
+            image.setAttribute("src", "images/icon-arrow-up.svg");
+        }
+        else{
+            image.setAttribute("src", "images/icon-arrow-down.svg");
+        }
+    });
+
+});
+
+let hamburger = document.querySelector(".hamburger");
+let navlinks = document.querySelector(".navbar .navlinks ul");
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navlinks.classList.toggle("active");
+    console.log("working")
+})
